1ed8282ed236d244d4b2d3ac717029bca93aa81e
saiteja Gangireddi <98887511+SaitejaGangireddi@users.noreply.github.com>
2026-08-15 04:01:03 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'tviojoadutmfztnutvou.supabase.co',
-        pathname: '/storage/v1/object/public/**',
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
