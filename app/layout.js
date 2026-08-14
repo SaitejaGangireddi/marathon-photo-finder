95702397718f62bc27aaf69e3f0c502e9daf1b8a
saiteja Gangireddi <98887511+SaitejaGangireddi@users.noreply.github.com>
2026-08-15 04:41:06 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,12 @@
 export const metadata = {
   title: 'Marathon Photo Finder',
-  description: 'Find your marathon race photos',
+  description: 'Find your marathon race photos by BIB or selfie',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body style={{ margin: 0, padding: 0, backgroundColor: '#f9fafb' }}>
         {children}
       </body>
     </html>
